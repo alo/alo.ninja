734423ec7504a6c8deb315147b5f5445a4018d8b
Álvaro Quirós <alvaro.quiros@gmail.com>
2020-03-16 20:26:06 +0100

    test: add some tests

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,33 @@
+/// <reference types="Cypress" />
+
+describe('Testing alo.dev page', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('should contain my name', () => {
+    cy.get('h1').contains('Álvaro Quirós');
+  });
+
+  it('should verify the color of the Projects section', () => {
+    cy.get('alo-projects > h2')
+      .should('have.css', 'color')
+      .and('eq', 'rgb(63, 81, 181)');
+  });
+
+  it('should open a modal to confirm personal social link and close it ', () => {
+    cy.get('alo-description > .mat-card > .mat-card-actions > alo-social-links ')
+      .find('.mat-icon-button > .mat-button-wrapper > .fa-instagram')
+      .click()
+      .get('alo-confirm-dialog')
+      .should('exist')
+      .get('.mat-dialog-actions > .mat-primary > .mat-button-wrapper')
+      .should('exist')
+      .contains('¡Vale!')
+      .get('button.mat-button > .mat-button-wrapper')
+      .contains('No, gracias')
+      .click()
+      .get('alo-confirm-dialog')
+      .should('not.exist');
+  });
+});
