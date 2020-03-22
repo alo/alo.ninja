9f1aee67d8da419c6dd5b6c9495c68afa3c16036
Álvaro Quirós <alvaro.quiros@gmail.com>
2020-03-22 22:22:33 +0100

    refactor: change project name to alodev

---
+++ scully.alodev.config.js
@@ -0,0 +1,26 @@
+const { MinifyHtml } = require('scully-minify-html');
+const { DisableAngular } = require('scully-disable-angular');
+
+const postRenderers = [MinifyHtml, DisableAngular];
+
+exports.config = {
+  projectRoot: './src',
+  defaultPostRenderers: postRenderers,
+  projectName: 'alodev',
+  outDir: './dist/static',
+  routes: {
+    // '/blog/:slug': {
+    //   type: 'contentFolder',
+    //   slug: {
+    //     folder: './blog'
+    //   }
+    // },
+    '/projects/:projectId': {
+      type: 'json',
+      projectId: {
+        url: 'https://www.coderty.com/projects.json',
+        property: 'projectId'
+      }
+    }
+  }
+};
